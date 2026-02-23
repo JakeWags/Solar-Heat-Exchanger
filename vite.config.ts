af237eaeaa34841ca033b597be5da9bdc459041d
Jake Wagoner <jakewagoneredu@gmail.com>
2026-02-23 09:53:33 -0700

    GH pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Solar-Heat-Exhcanger/',
   plugins: [react()],
   test: {
     environment: 'node',
